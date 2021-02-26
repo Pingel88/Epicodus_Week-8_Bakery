0fb19d8a0e997e72cc4f50d65793975b7d45cc17
Michael Pingel <mdpingel@gmail.com>
2021-02-26 08:21:41 -0800

    create initial file structure from a template

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+      // Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
+  }
+}
